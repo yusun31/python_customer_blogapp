1a3f956cb0fa5e265bc2ed12393c7418ec27ef81
yusun <yousun2286@naver.com>
2022-02-14 17:57:37 +0900

    urls

--- customer/urls.py
+++ customer/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.customer_list, name='customer_list'),
+    path('', views.customer_list, name='customer_list_home'),
+    path('customer/<int:pk>/', views.customer_detail, name='customer_detail'),
 ]
