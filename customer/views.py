f17dd52d64442039bba8a81ff94fc7cca1485157
yusun <yousun2286@naver.com>
2022-02-14 17:57:19 +0900

    views

--- customer/views.py
+++ customer/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+
+from .models import Customer
 
 
 # Create your views here.
 def customer_list(request):
-    return render(request, 'customer/customer_list.html')
+    cus = Customer.objects.filter(birthdate__lte=timezone.now()).order_by('-birthdate')
+    return render(request, 'customer/customer_list.html', {'customer_list': cus})
+
+
+def customer_detail(request, pk):
+    cu = get_object_or_404(Customer, pk=pk)
+    return render(request, 'customer/customer_detail.html', {'cus': cu})
