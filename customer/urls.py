ec01d8968cdd10ad556336d30011230d677c8e17
yusun <yousun2286@naver.com>
2022-02-15 16:38:27 +0900

    urls

--- customer/urls.py
+++ customer/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.customer_list, name='customer_list_home'),
     path('customer/<int:pk>/', views.customer_detail, name='customer_detail'),
+    path('customer/new/', views.customer_new, name='customer_new'),
 ]
