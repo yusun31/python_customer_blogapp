a7331858e9000b7d37406fc13c203c2c84d0c55f
yusun <yousun2286@naver.com>
2022-02-17 01:57:28 +0900

    forms

--- customer/forms.py
+++ customer/forms.py
@@ -1,5 +1,11 @@
 from django import forms
-from .models import Customer
+from .models import Customer, Comment
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
 
 
 def min_length_2_validator(value):
@@ -8,10 +14,21 @@ def min_length_2_validator(value):
         raise forms.ValidationError('이름은 2글자 이상 입력해주세요!')
 
 
-#  Modelform 상속받는 CustomerModelForm 클래스 정의
+#  Form 클래스
 class CustomerForm(forms.Form):
     name = forms.CharField(validators=[min_length_2_validator])
     email = forms.EmailField()
     birthdate = forms.DateField()
     choice = [('1', '남자'), ('0', '여자')]
     gender = forms.ChoiceField(widget=forms.RadioSelect, choices=choice)
+
+
+# Modelform 상속받는 CustomerModelForm 클래스 정의
+class CustomerModelForm(forms.ModelForm):
+    choice = [('1', '남자'), ('0', '여자')]
+    gender = forms.ChoiceField(widget=forms.RadioSelect, choices=choice)
+
+    class Meta:
+        model = Customer
+        fields = ('name', 'email', 'birthdate', 'gender',)
+        # fields = ['name', 'email', 'birthdate', 'gender']
