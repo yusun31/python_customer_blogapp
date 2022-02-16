a345cb1437cd2df17aecf4a495277d1c762e68d2
yusun <yousun2286@naver.com>
2022-02-17 01:58:02 +0900

    views

--- customer/views.py
+++ customer/views.py
@@ -1,7 +1,37 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
-from .forms import CustomerForm
-from .models import Customer
+from .forms import CustomerForm, CustomerModelForm, CommentForm
+from .models import Customer, Comment
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('customer_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment_pk = comment.post.pk
+    comment.delete()
+    return redirect('customer_detail', pk=comment_pk)
+
+
+def add_comment_to_customer(request, pk):
+    post = get_object_or_404(Customer, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('customer_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'customer/add_comment_to_customer.html', {'form': form})
 
 
 def customer_list(request):
@@ -14,6 +44,7 @@ def customer_detail(request, pk):
     return render(request, 'customer/customer_detail.html', {'cus': cu})
 
 
+@login_required
 def customer_new(request):
     if request.method == "POST":
         cust_form = CustomerForm(request.POST)
@@ -30,3 +61,23 @@ def customer_new(request):
     else:
         cust_form = CustomerForm()
     return render(request, 'customer/customer_edit.html', {'form': cust_form})
+
+
+@login_required
+def customer_edit(request, pk):
+    cust = get_object_or_404(Customer, pk=pk)
+    if request.method == 'POST':
+        cust_form = CustomerModelForm(request.POST, instance=cust)
+        if cust_form.is_valid():
+            customer = cust_form.save()
+            return redirect('customer_detail', pk=customer.pk)
+    else:
+        cust_form = CustomerModelForm(instance=cust)
+        return render(request, 'customer/customer_edit.html', {'form': cust_form})
+
+
+@login_required
+def customer_remove(request, pk):
+    cust = get_object_or_404(Customer, pk=pk)
+    cust.delete()
+    return redirect('customer_list_home')
