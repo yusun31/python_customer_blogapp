f5326d6eb33fb7982bd946f70cedab887e451fad
yusun <yousun2286@naver.com>
2022-02-17 01:57:52 +0900

    urls

--- customer/urls.py
+++ customer/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('', views.customer_list, name='customer_list_home'),
     path('customer/<int:pk>/', views.customer_detail, name='customer_detail'),
     path('customer/new/', views.customer_new, name='customer_new'),
+    path('customer/<int:pk>/edit/', views.customer_edit, name='customer_edit'),
+    path('customer/<int:pk>/remove/', views.customer_remove, name='customer_remove'),
+    path('customer/<int:pk>/comment/', views.add_comment_to_customer, name='add_comment_to_customer'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 ]
