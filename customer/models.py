f425267fd6d040a6390830a8d266f4f20c37de34
yusun <yousun2286@naver.com>
2022-02-17 01:57:38 +0900

    models

--- customer/models.py
+++ customer/models.py
@@ -2,6 +2,21 @@ from django.db import models
 from django.utils import timezone
 
 
+class Comment(models.Model):
+    post = models.ForeignKey('customer.Customer', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=100)
+    created_date = models.DateTimeField(default=timezone.now)
+    text = models.TextField()
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text + '(' + str(self.id) + ')'
+
+
 class Customer(models.Model):
     name = models.CharField(max_length=100)
     birthdate = models.DateField()
@@ -10,3 +25,6 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name + '(' + str(self.id) + ')'
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
